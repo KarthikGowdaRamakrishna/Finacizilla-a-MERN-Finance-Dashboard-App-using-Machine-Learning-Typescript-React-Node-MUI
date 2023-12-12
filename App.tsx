c5c65be2532f8dbc70db149d448315c5662f071e
KarthikGowdaRamakrishna <144963620+KarthikGowdaRamakrishna@users.noreply.github.com>
2023-12-12 02:55:49 -0500

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,35 @@
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+//import { create } from "domain";
+import { useMemo } from "react";
+import { themeSettings } from "./theme";
+import { Box, CssBaseline } from "@mui/material";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import  Navbar  from "./scenes/navbar";
+import Dashboard from "./scenes/dasboard";
+import Predictions from "./scenes/predictions";
+  
+
+function App() {
+  const theme = useMemo(()=> createTheme(themeSettings),[])
+  return( <div className="app">
+    <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <Box width="100%" height="100%"padding="1rem 2rem 4rem 2rem">
+      <Navbar/>
+      <Routes>
+<Route path ="/" element ={<Dashboard />}/>
+<Route 
+    path ="/predictions" 
+    element ={<Predictions />}/>
+
+
+        </Routes>
+      </Box>
+    </ThemeProvider>
+    </BrowserRouter>
+  </div>
+  );
+}
+
+export default App
